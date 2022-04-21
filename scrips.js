c7916ae6fff172840bd1cc870cdc086c04a995a4
oleksiy <super.alextim@gmail.com>
2022-04-21 23:54:16 +0300

    JS_2

---
+++ scrips.js
@@ -0,0 +1,12 @@
+/*=================== 1 task =================*/
+
+//const aNswer;
+
+const perfectNumber = (nUmber) => {
+  a = nUmber * (1 + 0.05 * 0.67);
+  return alert(a);
+};
+
+const nUmber = +prompt(`Enter number of money`);
+
+perfectNumber(nUmber);
